9189d7273728c103bd4d3c4700860b3ac1af04a3
Shams Haroon <shamsharoon7@gmail.com>
2023-10-25 17:42:22 -0400

    Added File

---
+++ lecture.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print()
+
+for i in [1,2,3,4]:
+    print(i)
+    
+print("glrtgit")
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
